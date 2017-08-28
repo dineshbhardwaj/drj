33e725c702a6cf868883ee7608f8de691eea1eeb
Apple <Apple@Apples-MacBook-Pro-2.local>
2017-08-28 13:25:20 +0530

    temp6

--- src/clojure_getting_started/web.clj
+++ src/clojure_getting_started/web.clj
@@ -15,9 +15,9 @@
    :body "Hello from Heroku"})
 
 (defn handler [request]
-  (response {:displayText "Bar"}))
-;;  (response {:speech "Turst me Deepak, It works !!"
-;;             :displayText "Turst me Deepak, It works !!"}))
+;;  (response {:displayText "Bar"}))
+  (response {:speech "Turst me Deepak, It works !!"
+             :displayText "Turst me Deepak, It works !!"}))
 
 (def app
   (wrap-json-response handler))
