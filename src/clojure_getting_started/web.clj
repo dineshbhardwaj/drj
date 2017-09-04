9cb10800ed34b64dbd9a4992b142b57667f44b42
Apple <Apple@Apples-MacBook-Pro-2.local>
2017-09-04 12:20:54 +0530

    temp

--- src/clojure_getting_started/web.clj
+++ src/clojure_getting_started/web.clj
@@ -6,6 +6,8 @@
             [ring.adapter.jetty :as jetty]
             [environ.core :refer [env]]
             [ring.middleware.json :refer [wrap-json-body]]
+            [ring.middleware.json :refer [json-response]]
+            [ring.middleware.json :refer [wrap-json-params]]
             [ring.middleware.json :refer [wrap-json-response]]
             [ring.util.response :refer [response]]
             [camel-snake-kebab.core :as kebab]))
@@ -16,46 +18,44 @@
    :body "Hello from Heroku"})
 
 
-(defn handler 
-;;   "generating different response depending on ans to 
-;;    if you know aricle or not"
-  [request]
-;;  (prn request)
-  (def input_data (get-in request [:body "yes, I have read the article"]))
-  (response {:speech input_data
-             :displayText "Turst me user, It works !!"}))
-;;  (response "Uploaded user.")
-
-
-(def app
-;;  (wrap-json-body handler {:keywords? true :bigdecimals true})
-  (wrap-json-response handler)
-  )
+;; not working(defn handler 
+;; not working;;   "generating different response depending on ans to 
+;; not working;;    if you know aricle or not"
+;; not working  [request]
+;; not working;;  (prn request)
+;; not working;;  (def input_data (get-in request [:body "resolveQuerry"]))
+;; not working  (response {:speech "hello"
+;; not working             :displayText "Turst me user, It works !!"})
+;; not working
+;; not working;; latter  (response {:speech input_data
+;; not working;; latter             :displayText "Turst me user, It works !!"})
+;; not working  ;;  (json-response response)
+;; not working  )
+;; not working
+;; not working;;  (response "Uploaded user.")
+;; not working
+;; not working
+;; not working(def app
+;; not working;;  (wrap-json-body handler {:keywords? true :bigdecimals true})
+;; not working  (wrap-json-response handler)
+;; not working                      )
 
 ;;(defn handler [request]
 ;;;;  (response {:displayText "Bar"}))
 ;;  (response {:speech "Turst me Deepak, It works !!"
 ;;             :displayText "Turst me Deepak, It works !!"}))
 
-
-;; working code for jason response (defn handler [request]
-;; working code for jason response ;;  (response {:displayText "Bar"}))
-;; working code for jason response   (process request)
-;; working code for jason response   (response {:speech "Turst me Deepak, It works !!"
-;; working code for jason response 
-;; working code for jason response (def app
-;; working code for jason response   (wrap-json-response handler))
 
 
 ;;(load-file "/Users/Apple/clojure-getting-started/src/clojure_getting_started/response.clj")
 
-;;(defn handler [request]
-;;;;  (response {:displayText "Bar"}))
-;;  (response {:speech "Turst me Deepak, It works !!"
-;;             :displayText "Turst me Deepak, It works !!"}))
-;;
-;;(def app
-;;  (wrap-json-response handler))
+;; was working (defn handler [request]
+;; was working ;;  (response {:displayText "Bar"}))
+;; was working   (response {:speech "Turst me Deepak, It works !!"
+;; was working              :displayText "Turst me Deepak, It works !!"}))
+;; was working 
+;; was working (def app
+;; was working   (wrap-json-response handler))
 ;;
 ;; addition from example 2nd 
 ;;(defroutes app
@@ -77,11 +77,11 @@
 ;;       (route/not-found (slurp (io/resource "404.html")))))
 
 ;; original example
-;;(defroutes app
-;;  (GET "/" []
-;;       (splash))
-;;  (ANY "*" []
-;;       (route/not-found (slurp (io/resource "404.html")))))
+(defroutes app
+  (GET "/" []
+       (splash))
+  (ANY "*" []
+       (route/not-found (slurp (io/resource "404.html")))))
 
 (defn -main [& [port]]
   (let [port (Integer. (or port (env :port) 5000))]
