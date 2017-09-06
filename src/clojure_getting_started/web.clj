7aec0ab96da68fe9b5984a3b9d0f53c8ba7c1edf
Apple <Apple@Apples-MacBook-Pro-2.local>
2017-09-06 19:17:06 +0530

    temp

--- src/clojure_getting_started/web.clj
+++ src/clojure_getting_started/web.clj
@@ -4,7 +4,7 @@
             [compojure.route :as route]
             [clojure.java.io :as io]
             [cheshire.core :as json]
-;;            [myapp.web.routes    :as api]
+            ;;            [myapp.web.routes    :as api]
             [ring.adapter.jetty :as jetty]
             [environ.core :refer [env]]
             [ring.middleware.json :refer [wrap-json-body]]
@@ -31,120 +31,89 @@
         default-handler (.getHandler server)]
     (.setHandler stats-handler default-handler)
     (.setHandler server stats-handler)
-;;    (.setStopTimeout  server a-minute)
+    ;;    (.setStopTimeout  server a-minute)
     (.setStopAtShutdown server true)))
 
 
 
-(defn json_converstion []
+;; not needed now (defn json_converstion []
+;; not needed now 
+;; not needed now   (def resp_data   (response {:speech "input_data" 
+;; not needed now                               :data_1 { :data_2 "hello" :data_3 "got"}
+;; not needed now                               :displayText "Turst me user, It works !!"}))
+;; not needed now   (def json_resp_data  (json-response resp_data  {}))
+;; not needed now   (if-let [request (json-params-request json_resp_data {:bigdecimals true} )]
+;; not needed now ;;request
+;; not needed now     (get (json/decode (get-in request [:body])) "data_1")
+;; not needed now  )
+;; not needed now ;; older code   (if-let [request (json-body-request json_resp_data {:keywords? true :bigdecimals true} )]
+;; not needed now ;; older code     ;;  (def input_data (get-in  (json-body-request (get-in request [:body :originalRequest]) {:keywords? true :bigdecimals true}) [:source]))
+;; not needed now ;; older code     ;;  (def input_data (get-in  (get-in request [:body :result]) [:resolveQuerry]))
+;; not needed now ;; older code ;;    (:speech   (json/parse-string (get-in request [:body])))
+;; not needed now ;; older code ;;   (get-in request [:body])
+;; not needed now ;; older code  (get (json/decode (get-in request [:body])) "data_1")
+;; not needed now ;; older code ;;(json/decode  (get-in request [:body]) {:keywords? true :bigdecimals true})
+;; not needed now ;; older code ;;(json/decode "{\"data_1\":{\"data_2\":\"hello\",\"data_3\":\"got\"},\"displayText\":\"Turst me user, It works !!\",\"speech\":\"input_data\"}") 
+;; not needed now ;; older code  )
+;; not needed now )
 
-  (def resp_data   (response {:speech "input_data" 
-                              :data_1 { :data_2 "hello" :data_3 "got"}
-                              :displayText "Turst me user, It works !!"}))
-  (def json_resp_data  (json-response resp_data  {}))
-  (if-let [request (json-params-request json_resp_data {:bigdecimals true} )]
-;;request
-    (get (json/decode (get-in request [:body])) "data_1")
- )
-;; older code   (if-let [request (json-body-request json_resp_data {:keywords? true :bigdecimals true} )]
-;; older code     ;;  (def input_data (get-in  (json-body-request (get-in request [:body :originalRequest]) {:keywords? true :bigdecimals true}) [:source]))
-;; older code     ;;  (def input_data (get-in  (get-in request [:body :result]) [:resolveQuerry]))
-;; older code ;;    (:speech   (json/parse-string (get-in request [:body])))
-;; older code ;;   (get-in request [:body])
-;; older code  (get (json/decode (get-in request [:body])) "data_1")
-;; older code ;;(json/decode  (get-in request [:body]) {:keywords? true :bigdecimals true})
-;; older code ;;(json/decode "{\"data_1\":{\"data_2\":\"hello\",\"data_3\":\"got\"},\"displayText\":\"Turst me user, It works !!\",\"speech\":\"input_data\"}") 
-;; older code  )
-)
-
-  (defn serialize [m sep] (str (clojure.string/join sep (map (fn [[_ v]] _ v) m)) "\n"))
 
 (defn handler 
-;;   "generating different response depending on ans to 
-;;    if you know aricle or not"
+  ;;   "generating different response depending on ans to 
+  ;;    if you know aricle or not"
   [request]
-;;  (prn request)
-;; yes (if-let [request (json-body-request request {:keywords? true :bigdecimals true} )]
-;; yes ;;  (def input_data (get-in  (json-body-request (get-in request [:body :originalRequest]) {:keywords? true :bigdecimals true}) [:source]))
-;; yes ;;  (def input_data (get-in  (get-in request [:body :result]) [:resolveQuerry]))
-;; yes   (def input_data  (get-in request [:body "result" "resolvedQuerry"])))
-;; yes ;;  (def input_data (get  (get  (json/decode (get-in request [:body :result])) "result") "resolvedQuerry")) 
-;;
-;; hold on (def res_wo_json
-;; hold on     (response {:speech input_data
-;; hold on                :displayText "Turst me user, It works !!"})
-;; hold on     )
+  (def input_data  (str  (get  (get-in (json-body-request request {:keywords? true :bigdecimals true}) [:body :result])  :resolvedQuery)))
+  (response {:speech input_data
+             :displayText "Turst me user, It works !!"})
+  )
 
 
-;; worked wrong (def input_data   (str  (json-body-request (json-response (str (get-in (json-body-request request {:keywords? true :bigdecimals true}) [:body :result]))  {:keywords? true :bigdecimals true}) [:body :resolvedQuerry]  )  ))
-;;(def input_data   (str  (get-in (json-body-request (json-response (str ":result" (get-in (json-body-request request {:keywords? true :bigdecimals true}) [:body :result])) {}) {:keywords? true :bigdecimals true}) [:result :source]  ))  )
-;;(def input_data   (str  (get-in (json-body-request (json-response (str ":result " (get-in (json-body-request request {:keywords? true :bigdecimals true}) [:body :result])) {}) {:keywords? true :bigdecimals true}) [:result :source]  ))  )
-;;(def input_data  (str   (get-in (json-body-request request {:keywords? true :bigdecimals true}) [:body :result])  ))
-(def input_data  (str  (get  (get-in (json-body-request request {:keywords? true :bigdecimals true}) [:body :result])  :resolvedQuery)))
-;;(def input_data  (str (get  (get-in (json-body-request request {:keywords? true :bigdecimals true}) [:body :result]) :source)))
-;;(def input_data   (str (get-in (json/decode (json-body-request (json-response (str (get-in (json-body-request request {:keywords? true :bigdecimals true}) [:body :result]))  {:keywords? true :bigdecimals true}) [:body :resolvedQuerry]  )) :resolvedQuerry) ))
-;;(def input_data (serialize (get-in  (json-body-request request {:keywords? true :bigdecimals true}) [:body]) ,)) 
- ;;(def input_data  (get-in request (json-params-request request { :bigdecimals true}) [:params "timestamp"]))
+(def app
+  ;;  (wrap-json-body handler {:keywords? true :bigdecimals true})
+  (wrap-json-response handler)
+  )
 
 
-               (response {:speech input_data
-                          :displayText "Turst me user, It works !!"}))
+;;(load-file "/Users/Apple/clojure-getting-started/src/clojure_getting_started/response.clj")
 
-          ;;  (response "Uploaded user.")
+;; response back working  (defn handler [request]
+;; response back working  ;;  (response {:displayText "Bar"}))
+;; response back working    (response {:speech "Turst me Deepak, It works !!"
+;; response back working               :displayText "Turst me Deepak, It works !!"}))
+;; response back working  
+;; response back working  (def app
+;; response back working    (wrap-json-response handler))
 
+;; addition from example 2nd 
+;;second example (defroutes app
+;;second example   (GET "/camel" {{input :input} :params}
+;;second example        {:status 200
+;;second example         :headers {"Content-Type" "text/plain"}
+;;second example         :body (kebab/->CamelCase input)})
+;;second example   (GET "/snake" {{input :input} :params}
+;;second example        {:status 200
+;;second example         :headers {"Content-Type" "text/plain"}
+;;second example         :body (kebab/->snake_case input)})
+;;second example   (GET "/kebab" {{input :input} :params}
+;;second example        {:status 200
+;;second example         :headers {"Content-Type" "text/plain"}
+;;second example         :body (kebab/->kebab-case input)})
+;;second example   (GET "/" []
+;;second example        (splash))
+;;second example   (ANY "*" []
+;;second example        (route/not-found (slurp (io/resource "404.html")))))
 
-          (def app
-            ;;  (wrap-json-body handler {:keywords? true :bigdecimals true})
-            (wrap-json-response handler)
-            )
+;; original example
+;; first example (defroutes app
+;; first example   (GET "/" []
+;; first example        (splash))
+;; first example   (ANY "*" []
+;; first example        (route/not-found (slurp (io/resource "404.html")))))
 
-          ;;(defn handler [request]
-;;;;  (response {:displayText "Bar"}))
-          ;;  (response {:speech "Turst me Deepak, It works !!"
-          ;;             :displayText "Turst me Deepak, It works !!"}))
+(defn -main [& [port]]
+  (let [port (Integer. (or port (env :port) 5000))]
+    (jetty/run-jetty (site #'app) {:port port :join? false :configurator conf})))
 
-
-
-          ;;(load-file "/Users/Apple/clojure-getting-started/src/clojure_getting_started/response.clj")
-
-          ;; response back working  (defn handler [request]
-          ;; response back working  ;;  (response {:displayText "Bar"}))
-          ;; response back working    (response {:speech "Turst me Deepak, It works !!"
-          ;; response back working               :displayText "Turst me Deepak, It works !!"}))
-          ;; response back working  
-          ;; response back working  (def app
-          ;; response back working    (wrap-json-response handler))
-
-          ;; addition from example 2nd 
-          ;;second example (defroutes app
-          ;;second example   (GET "/camel" {{input :input} :params}
-          ;;second example        {:status 200
-          ;;second example         :headers {"Content-Type" "text/plain"}
-          ;;second example         :body (kebab/->CamelCase input)})
-          ;;second example   (GET "/snake" {{input :input} :params}
-          ;;second example        {:status 200
-          ;;second example         :headers {"Content-Type" "text/plain"}
-          ;;second example         :body (kebab/->snake_case input)})
-          ;;second example   (GET "/kebab" {{input :input} :params}
-          ;;second example        {:status 200
-          ;;second example         :headers {"Content-Type" "text/plain"}
-          ;;second example         :body (kebab/->kebab-case input)})
-          ;;second example   (GET "/" []
-          ;;second example        (splash))
-          ;;second example   (ANY "*" []
-          ;;second example        (route/not-found (slurp (io/resource "404.html")))))
-
-          ;; original example
-          ;; first example (defroutes app
-          ;; first example   (GET "/" []
-          ;; first example        (splash))
-          ;; first example   (ANY "*" []
-          ;; first example        (route/not-found (slurp (io/resource "404.html")))))
-
-          (defn -main [& [port]]
-            (let [port (Integer. (or port (env :port) 5000))]
-              (jetty/run-jetty (site #'app) {:port port :join? false :configurator conf})))
-
-          ;; For interactive development:
-          ;; (.stop server)
-          (defonce server (clojure-getting-started.web/-main))
+;; For interactive development:
+;; (.stop server)
+(defonce server (clojure-getting-started.web/-main))
