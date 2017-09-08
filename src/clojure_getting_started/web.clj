f1fe03278c890a5a3b8f28910bd6a642b3258d18
Apple <Apple@Apples-MacBook-Pro-2.local>
2017-09-08 14:26:14 +0530

    temp

--- src/clojure_getting_started/web.clj
+++ src/clojure_getting_started/web.clj
@@ -84,7 +84,9 @@
 
 (defn -main [& [port]]
   (let [port (Integer. (or port (env :port) 5000))]
-    (jetty/run-jetty (site #'app) {:port port :join? false :configurator conf})))
+;;    (jetty/run-jetty (site #'app) {:port port :join? false :configurator conf})
+    (jetty/run-jetty (site #'app) {:port port :join? false})
+))
 
 ;; For interactive development:
 ;; (.stop server)
