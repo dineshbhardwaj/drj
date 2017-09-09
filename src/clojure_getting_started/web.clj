0ca12e55746d11ba9e7bcc4e1b178c65b31c9320
Apple <Apple@Apples-MacBook-Pro-2.local>
2017-09-09 15:06:36 +0530

    temp

--- src/clojure_getting_started/web.clj
+++ src/clojure_getting_started/web.clj
@@ -22,15 +22,15 @@
 ;;  (def input_context  (str  (get  (get-in (json-body-request request {:keywords? true :bigdecimals true}) [:body :result])  :contexts)))
   (def input_context  (str   (get  (get-in (json-body-request request {:keywords? true :bigdecimals true}) [:body :result])  :contexts)))
   (if (.contains input_context "{:name \"got_article_defination\", :parameters {}, :lifespan 5}")
-;;;;    (do  
+    (do  
       (def input_data  (str  (get  (get-in (json-body-request request {:keywords? true :bigdecimals true}) [:body :result])  :resolvedQuery)))
       ;;      (def aritcle_deination_regexp (article-defination))
-;;;; wait      (if  (match-regexp-string-list input_data aritcle_deination_regexp)
-;;;; wait        (def input_data "your Article defination looks ok. We declared Article as a or an and the.")
-;;;; wait        (def input_data "your Article defination does not seems to be correct. Article as a or an and the."))
+      (if  (match-regexp-string-list input_data aritcle_deination_regexp)
+        (def input_data "your Article defination looks ok. We declared Article as a or an and the.")
+        (def input_data "your Article defination does not seems to be correct. Article as a or an and the."))
       )
     (def input_data (str "your context did not match expected. Input context is : " input_context ))
-    ;;)
+    )
     (response {:speech input_data
                :displayText "Turst me user, It works !!"}))
 
