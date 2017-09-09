a378c3211a4e6f08d4169ee2a8fb9c18b2aba0d9
Apple <Apple@Apples-MacBook-Pro-2.local>
2017-09-09 16:13:48 +0530

    temp

--- src/clojure_getting_started/web.clj
+++ src/clojure_getting_started/web.clj
@@ -21,7 +21,8 @@
   (if (.contains input_context "{:name \"got_article_defination\", :parameters {}, :lifespan 5}")
     (do  
       (def input_data  (str  (get  (get-in (json-body-request request {:keywords? true :bigdecimals true}) [:body :result])  :resolvedQuery)))
-      (def aritcle_deination_regexp   (list #"a.*an.*the" #"a.*the.*an" #"an.*a.*the" #"an.*the.*a"  #"the.*an.*a" #"the.*a.*an"))
+;;      (def aritcle_deination_regexp   (list #"a.*an.*the" #"a.*the.*an" #"an.*a.*the" #"an.*the.*a"  #"the.*an.*a" #"the.*a.*an"))
+      (def aritcle_deination_regexp   (article-def))
 ;;    (if  (match-regexp-string-list input_data aritcle_deination_regexp)
         (def input_data "your Article defination looks ok. We declared Article as a or an and the.")
         (def input_data "your Article defination does not seems to be correct. Article as a or an and the.")
@@ -32,7 +33,7 @@
     (response {:speech input_data
                :displayText "Turst me user, It works !!"}))
 
-(defn article-def
+ (defn article-def
   []
   (list #"a.*an.*the" #"a.*the.*an"
   #"an.*a.*the" #"an.*the.*a"
